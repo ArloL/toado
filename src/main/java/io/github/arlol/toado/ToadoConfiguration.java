951ccd99477ad7e8ebb326580ee843935199d0d4
Arlo O'Keeffe <mailtoarlo@gmail.com>
2019-12-04 11:21:55 +0100

    Remove TODO and unnecessary imports

--- src/main/java/io/github/arlol/toado/ToadoConfiguration.java
+++ src/main/java/io/github/arlol/toado/ToadoConfiguration.java
@@ -1,10 +1,7 @@
 package io.github.arlol.toado;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
 
 public class ToadoConfiguration extends Configuration {
-    // TODO: implement service configuration
+
 }
